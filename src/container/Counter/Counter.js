71027fa7315f04ac9e78b168b2161793629aaafe
Zeroitman <borsh130894@gmail.com>
2019-03-27 23:27:34 +0600

    Добавлены функции ввода чисел, удаления и проверку на соответствие

--- src/container/Counter/Counter.js
+++ src/container/Counter/Counter.js
@@ -4,27 +4,33 @@ import {connect} from "react-redux";
 
 class Counter extends Component {
     render() {
+        // const array_numbers = this.props.array_numbers;
+        console.log(this.props.comparison);
+        console.log(this.props.array_numbers);
+        console.log(this.props.password);
         return (
             <div className="mt-5">
                 <div className="display_block">
-                    <h1>{this.props.counter}</h1>
+                    {/*{array_numbers.length === 0 ?*/}
+                    {/*<div className="mt-2">Введите пароль</div> */}
+                    {/*: */}
+                    <h1>{this.props.array_numbers}</h1>
+                    {/*}*/}
+
                 </div>
                 <div className="enter_block mt-1">
-                    <button className="block" onClick="">{this.props.one}</button>
-                    <button className="block" onClick="">{this.props.two}</button>
-                    <button className="block" onClick="">{this.props.three}</button>
-                    <button className="block" onClick="">{this.props.four}</button>
-                    <button className="block" onClick="">{this.props.five}</button>
-                    <button className="block" onClick="">{this.props.six}</button>
-                    <button className="block" onClick="">{this.props.seven}</button>
-                    <button className="block" onClick="">{this.props.eight}</button>
-                    <button className="block" onClick="">{this.props.nine}</button>
-                    <button className="block" onClick="">{this.props.back}</button>
-                    <button className="block" onClick="">{this.props.zero}</button>
-                    <button className="block" onClick="">{this.props.enter}</button>
-                    {/*<button onClick={this.props.decreaseCounter}>Decrease</button>*/}
-                    {/*<button onClick={this.props.addCounter}>Increase by 5</button>*/}
-                    {/*<button onClick={this.props.subtractCounter}>Decrease by 5</button>*/}
+                    <button className="block" id="1" value={1} onClick={()=> this.props.show(1)}>1</button>
+                    <button className="block" id="2" value={2} onClick={()=> this.props.show(2)}>2</button>
+                    <button className="block" id="3" value={3} onClick={()=> this.props.show(3)}>3</button>
+                    <button className="block" id="4" value={4} onClick={()=> this.props.show(4)}>4</button>
+                    <button className="block" id="5" value={5} onClick={()=> this.props.show(5)}>5</button>
+                    <button className="block" id="6" value={6} onClick={()=> this.props.show(6)}>6</button>
+                    <button className="block" id="7" value={7} onClick={()=> this.props.show(7)}>7</button>
+                    <button className="block" id="8" value={8} onClick={()=> this.props.show(8)}>8</button>
+                    <button className="block" id="9" value={9} onClick={()=> this.props.show(9)}>9</button>
+                    <button className="block" id="10" value="" onClick={this.props.delete}>Back</button>
+                    <button className="block" id="0" value={0} onClick={()=> this.props.show(0)}>0</button>
+                    <button className="block" id="11" value="" onClick={this.props.check}>Ent</button>
                 </div>
             </div>
         );
@@ -33,28 +39,23 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        counter: state.counter,
-        one: 1,
-        two: 2,
-        three: 3,
-        four: 4,
-        five: 5,
-        six: 6,
-        seven: 7,
-        eight: 8,
-        nine: 9,
-        zero: 0,
-        back: "<",
-        enter: "ENT"
+        password: state.password,
+        comparison: state.comparison,
+        array_numbers: state.array_numbers
     };
 };
 
+
 const mapDispatchToProps = dispatch => {
+    function daiValue(id) {
+        let values = document.getElementById(id).value;
+        return Number.parseInt(values);
+    }
+
     return {
-        increaseCounter: () => dispatch({type: 'INCREMENT'}),
-        decreaseCounter: () => dispatch({type: 'DECREMENT'}),
-        addCounter: () => dispatch({type: 'ADD', value: 5}),
-        subtractCounter: () => dispatch({type: 'SUBTRACT', value: 5}),
+        show: (id) => dispatch({type: 'ENTER', value: daiValue(id)}),
+        delete: () => dispatch({type: 'DELETE'}),
+        check: () => dispatch({type: 'CHECK'}),
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,16 +1,36 @@
 const initialState = {
-    counter: 10
+    password: [1, 2, 3, 4],
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    zero: 0,
+    back: "<",
+    enter: "ENT",
+    array_numbers: [1, 2],
+    comparison: false
 };
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {counter: state.counter + 1};
+
+    if (action.type === 'ENTER') {
+        let Array = state.array_numbers;
+        Array.push(action.value);
+        return {array_numbers: Array};
     }
-    if (action.type === 'DECREMENT') {
-        return {counter: state.counter - 1};
+    if (action.type === 'DELETE') {
+        let Array = state.array_numbers;
+        Array.pop();
+        return {array_numbers: Array};
     }
-    if (action.type === 'ADD') {
-        return {counter: state.counter + action.value};
+    if (action.type === 'CHECK') {
+        let compare = JSON.stringify(state.array_numbers) === JSON.stringify(state.password);
+        return {comparison: compare};
     }
     if (action.type === 'SUBTRACT') {
         return {counter: state.counter - action.value};
